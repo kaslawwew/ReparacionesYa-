baa8a6adf6bf54869b25f1aeb3a56b641cfb415c
kaslawwew <abelkal1306@gmail.com>
2026-05-01 19:11:46 -0300

    Fix Vercel serverless function: use CommonJS export and simplify config

--- api/reparacion.js
+++ api/reparacion.js
@@ -175,7 +175,7 @@ const htmlExito = (datos) => `
 </html>
 `;
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
